0d10bb1d30c53a6edc2a5d6556c1b00f09f65a5b
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-07-09 11:25:39 -0700

    Add an update script
    This script simply updates the committed binaries.

---
+++ update.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Configuration bits...
+CHISEL_HASH="333bb95fd6e2780d6f3629699497953e333ef001"
+
+# Reference the main directory, for later
+TOP="$(pwd)"
+SRC="$TOP"/src
+
+# Create a temporary directory in which we'll be doing all the dirty
+# work
+TMP="$(mktemp -d -t chisel-benchmarks.XXXXXXXXXX)"
+trap "rm -rf $TMP" EXIT
+
+mkdir -p "$SRC"
+
+# First we go ahead and clone the main Chisel repository
+cd $TMP
+git clone git://github.com/ucb-bar/chisel.git --reference /var/cache/git
+
+# Build a copy of Chisel so we can obtain the JAR file
+cd "$TMP"/chisel
+git reset --hard $CHISEL_HASH
+sbt publish-local
+
+# Copy that JAR file out into the target directory.
+cd "$TMP"/chisel/
+cat "$(find -name "chisel_*-SNAPSHOT.jar")" | base64 > "$SRC"/chisel-jar.bash
+
+# Also attempt to generate a tarball of the headers that come out of
+# the Chisel distribution, for later use.
+cd "$TMP"/chisel/src/main/resources/
+find -iname "*.h" | xargs tar -c | base64 > "$SRC"/headers-tar.bash
